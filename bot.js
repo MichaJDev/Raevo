640a45a77accbc0f04791fff87835d7fd4f7072d
Micha Janssen Desktop <michajanssen93@gmail.com>
2019-09-13 23:43:32 +0200

    Added some necessary check commands Debug Commands

--- bot.js
+++ bot.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const auth = require('./config.json');
 let TOKEN = auth.token;
+const prefix = "-?";
 
 client.on('ready', () => {
   console.log('Logged in as ${client.user.tag}!');
 });
 client.on('message', async message => {
-  const args = message.content.slice(auth.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
+  if (message.author.bot) return;
   //Automatic Reply for Listing server
   if (message.content.includes('discord.gg/')) {
     message.channel.send({
@@ -31,61 +33,59 @@ client.on('message', async message => {
       }
     });
   }
-    if(message.author.bot) return;
-    if(message.content.indexOf(auth.prefix) !== 0) return;
-    //ping command
-    if (command === auth.prefix +'ping') {
-      message.reply('pong');
+  var server = message.guild.id;
+  //ping command
+  if (command === 'ping') {
+    message.reply('pong');
+  }
+  if (command === 'guild') {
+
+    message.reply(" the server id: " + server);
+  }
+  if(command === 'owner'){
+    var guild = client.guilds.get(server);
+    message.reply(guild.owner);
+  }
+  if (command === 'ban') {
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (message.author.roles.some(role => role.name === 'Owner')) {
+      if (user) {
+        // Now we get the member from the user
+        const member = message.guild.member(user);
+        // If the member is in the guild
+        if (member) {
+          /**
+           * Ban the member
+           * Make sure you run this on a member, not a user!
+           * There are big differences between a user and a member
+           * Read more about what ban options there are over at
+           * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
+           */
+          member.ban({
+            reason: 'They were bad!',
+          }).then(() => {
+            // We let the message author know we were able to ban the person
+            message.reply('Successfully banned ${user.tag}');
+            member.sendMessage('You have banned from ' + message.guild.name + member.name + "| Reason: Undefined");
+          }).catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to ban the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to ban the member');
+            // Log the error
+            console.error(err);
+          });
+        } else {
+          // The mentioned user isn't in this guild
+          message.reply('That user isn\'t in this guild!');
+        }
+      } else {
+        // Otherwise, if no user was mentioned
+        message.reply('You didn\'t mention the user to ban!');
+      }
     }
-    //ban command
-    if(command === "kick") {
-      // This command must be limited to mods and admins. In this example we just hardcode the role names.
-      // Please read on Array.some() to understand this bit:
-      // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-      if(!message.member.roles.some(r=>["Owner", "Administrator", "Moderator"].includes(r.name)) || !message.member.id !== "Vyx#3045" )
-        return message.reply("Sorry, you don't have permissions to use this!");
-
-      // Let's first check if we have a member and if we can kick them!
-      // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-      // We can also support getting the member by ID, which would be args[0]
-      let member = message.mentions.members.first() || message.guild.members.get(args[0]);
-      if(!member)
-        return message.reply("Please mention a valid member of this server");
-      if(!member.kickable)
-        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-
-      // slice(1) removes the first part, which here should be the user mention or ID
-      // join(' ') takes all the various parts to make it a single string.
-      let reason = args.slice(1).join(' ');
-      if(!reason) reason = "No reason provided";
-
-      // Now, time for a swift kick in the nuts!
-      await member.kick(reason)
-        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-    }
-    //Kick command
-    if(command === "ban") {
-     // Most of this command is identical to kick, except that here we'll only let admins do it.
-     // In the real world mods could ban too, but this is just an example, right? ;)
-     if(!message.member.roles.some(r=>["Owner, Administrator"].includes(r.name)) )
-       return message.reply("Sorry, you don't have permissions to use this!");
-
-     let member = message.mentions.members.first();
-     if(!member)
-       return message.reply("Please mention a valid member of this server");
-     if(!member.bannable)
-       return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
-
-     let reason = args.slice(1).join(' ');
-     if(!reason) reason = "No reason provided";
-
-     await member.ban(reason)
-       .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
-     message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
-   }
-
+  }
 });
 
 
